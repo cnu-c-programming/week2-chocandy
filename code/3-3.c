3d47212b8a3d78adcf5750ad8d2c6c845a901096
chominjeong <chomj4836@gmail.com>
2026-03-19 18:10:20 +0900

    Update 2-2 ~ 4-3

--- code/3-3.c
+++ code/3-3.c
@@ -2,7 +2,8 @@
 
 int main()
 {
+    printf("%d\n", 5/3);
+    printf("%f\n", (float)5/3);
 
     return 0;
 }
-
--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,23 @@
 
 int main()
 {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    if (op == '+') {
+        printf("%d\n", a + b);
+    }
+    else if (op == '-') {
+        printf("%d\n", a - b);
+    }
+    else if (op == '*') {
+        printf("%d\n", a * b);
+    }
+    else if (op == '/') {
+        printf("%d\n", a / b);
+    }
 
     return 0;
 }
-
